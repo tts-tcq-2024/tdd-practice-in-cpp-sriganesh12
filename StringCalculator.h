7b62e2279ed007da4a455b4ace5f7bd18054b962
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-09-19 11:24:47 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,7 +6,7 @@
 class StringCalculator
 {
 public:
-int add (const std::string& name);
+int add(const std::string& name);
 private:
 void checkForException(const std::string& name);
 void checkNegativeNumbers(const std::string& name);
