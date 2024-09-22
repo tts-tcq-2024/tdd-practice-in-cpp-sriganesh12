46c487abf3ee14529c57b42048d5bf54c90193a2
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-09-22 19:42:34 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,34 +1,56 @@
 #include "StringCalculator.h"
-include <string>
+#include <string>
 #include <cctype>
 #include <sstream>
 #include <iostream>
+#include <stdexcept>
 
-int add(const std::string& name)
+void StringCalculator::checkForException(const std::string& item)
 {
-  if(name.empty())
-  {
-    return 0;
-  }
-  if(name.substr(0,2) == "//")
-  {
-    char delimiter = name[2];
-    name = name.substr(4);
-    str::replace(name.begin(), name.end(), delimiter, ',');
-  }
-  str::replace(name.begin(), name.end(), '\n', ',');
+    for(char ch : item)
+      if(isalpha(ch))
+      {
+          std::string message = "Alphabets not allowed: ";
+          std::runtime_error(message)
+      }
+}
 
+int StringCalculator::addNumbersOnly(const std::string& item)
+{
+  int value = std::stoi(item);
+  if(value > 0 && value <= 1000)
+  {
+    return value;
+  }
+  return 0;
+}
+
+void StringCalculator::prepareString(const std::string& name)
+{
+  if(!name.empty())
+  {
+    if(name.substr(0,2) == "//")
+    {
+      char delimiter = name[2];
+      name = name.substr(4);
+      str::replace(name.begin(), name.end(), delimiter, ',');
+    }
+    str::replace(name.begin(), name.end(), '\n', ',');
+  }
+}
+
+int StringCalculator::add(const std::string& name)
+{
+  prepareString(name);
   std::stringstream ss(name);
   std::string item;
   int sum = 0;
 
   while (std::getline(ss, item, ','))
   {
-    int value += std::stoi(item);
-    if (value>0 && value<=1000)
-    {
+    checkForException(item);
+    int value = addNumbersOnly(item);
     sum += value;
-    }
   }
 
   return sum;
